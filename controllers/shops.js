19c2beb75257999c6f82eb4da74a4be48a2ecca0
DidacPH <magozero942.c1@gmail.com>
2017-09-15 11:08:36 +0200

    shops
    controller shops, modificacion del http

--- controllers/shops.js
+++ controllers/shops.js
@@ -1,30 +1,19 @@
 angular.module('spaApp')
-.controller('ShopsCtrl', function($scope, $http) {
-	$http({
-		method : "GET",
-		url : "models/shops.php"
-	}).then(function mySucces(response) {
-		$scope.names = response.data;
-		}, function myError(response) {
-		$scope.names = response.statusText;
-	});
-	console.log($scope.names);
-	/*$scope.showCategoriaData = function($idCategoria = 0){		
-		$scope.loading = true;
-		$scope.muestraDataCategorias = false;
-		$scope.divCategories = false;
+
+  .controller('ShopsCtrl', function($scope, $http) {
+
+
 		$http({
 			method : "GET",
-			url : "../controller/categorias.php?acc=m&idCategoria="+$idCategoria
-		}).then(function mySucces(response) {
-			$scope.names_category = response.data;
-			$scope.loading = false;
-			$scope.muestraDataCategorias = true;
-		}, function myError(response) {
-			$scope.names_category = response.statusText;
+			url : "models/shops.php"
+		}).then(function mySucces (response) {
+			$scope.names = response.data;
+			
+		}, function myError (response) {
+			$scope.names = response.statusText;
 		});
-	};*/
-});
+			
+	});
 
 /*if (isset($_GET['acc']) && $_GET['acc'] == 'l')
 {
