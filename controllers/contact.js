3fcdaac43a1193a79c73dc3392cb4dcd29293198
Nio94 <neofito94@gmail.com>
2017-09-15 14:41:10 +0200

    INDEX
    Parametros enviados y contact.js eliminado

--- controllers/contact.js
+++
@@ -1,6 +0,0 @@
-angular.module('spaApp')
-  .controller('ContactCtrl', function ($scope) {
-  	$scope.contact = "EixBcn@gmail.com";				// ---- ESPERANZAS Y SUEÑOS ROTOS ----
-  	  	// TODO Relación idUser -> Email 
- });
-
