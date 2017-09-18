c525605f057bbcc50e8869f6e52926db7bde85cf
DidacPH <magozero942.c1@gmail.com>
2017-09-18 09:40:37 +0200

    cambios para associaciones
    shops controller, añadido funciones en users.js y modificado
    associations.html

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 angular.module('spaApp')
 
-  .controller('aboutUsCtrl', function($scope, $http) {
+  .controller('AboutUsCtrl', function($scope, $http) {
 
 
 		$http({
@@ -31,4 +31,41 @@ $http({
 			$scope.email = response.statusText;
 		});
   console.log('Llega email'+$scope.email);
+});
+
+angular.module('spaApp')
+
+.controller('AssociationsCtrl', function($scope, $http) {
+
+  	$scope.showHistory = function(idUser){
+		$http({
+			method : "GET",
+			url : "models/users.php?idUser="+idUser
+		}).then(function mySucces (response) {
+			templateUrl:'view/aboutUs.html'
+			$scope.names = response.data;
+			$scope.div4 = false;
+			$scope.div3 = false;
+			$scope.div1 = false;
+			$scope.div2 = true;
+		}, function myError (response) {
+			$scope.names = response.statusText;
+		});
+	};
+
+	$scope.showContact = function(idUser){
+		$http({
+			method : "GET",
+			url : "models/users.php?acc=mail&idUser="+ $scope.idUser
+		}).then(function mySucces (response) {
+			templateUrl:'view/contact.html'
+			$scope.email = response.data;
+			$scope.div2 = false;
+			$scope.div3 = false;
+			$scope.div1 = false;
+			$scope.div4 = true;
+		}, function myError (response) {
+			$scope.email = response.statusText;
+		});
+	};
 });
