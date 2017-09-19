0509c999db64d9d622d7950f9c908bf582b08998
username(Jordi) <email>
2017-09-19 09:31:38 +0200

    Tabulado

--- models/users.php
+++ models/users.php
@@ -3,21 +3,18 @@
 require("../inc/functions.php");
 
 	if(isset($_GET['acc'])&&$_GET['acc']=='history'){
-	
 		$mySql = "SELECT history FROM users  WHERE idUser=".$_GET["idUser"];
 		$connexio = connect();
 		$resultAboutUs = mysqli_query($connexio, $mySql);
 		disconnect($connexio);
-
 			while ($row=mySqli_fetch_array($resultAboutUs))
 			{
 				
 				$historyAboutUs=$row['history'];
 			}
-
 		echo $historyAboutUs;
-
 	}
+
  	else if (isset($_GET["acc"]) && $_GET["acc"] == "mail") {
  		$mySql = "SELECT email FROM users WHERE idUser=".$_GET["idUser"];
 		$connexio = connect();
