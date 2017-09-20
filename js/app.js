48249a7de348ee213412c2680c72e2637260169b
Nio94 <neofito94@gmail.com>
2017-09-20 14:24:31 +0200

    CORECCION INDEX Y APP

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var app= angular.module('spaApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-    .when('/', {
-        templateUrl: 'views/comercialMap.html',
-        controller: 'ComercialMapCtrl',
-    })
+    // .when('/', {
+    //     templateUrl: 'views/comercialMap.html',
+    //     controller: 'ComercialMapCtrl',
+    // })
 	.when('/news/:idUser',{
 	templateUrl:'views/news.html',
 	controller:'NewsCtrl',
@@ -14,7 +14,7 @@ app.config(['$routeProvider', function ($routeProvider) {
 		templateUrl:'views/promotions.html',
 		controller:'PromotionsCtrl',
 	})
-	.when('/associations',{
+	.when('/associations/:idUser',{
 		templateUrl:'views/associations.html',
 		controller:'AssociationsCtrl',
 	})
