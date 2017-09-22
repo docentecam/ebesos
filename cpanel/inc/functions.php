448d32a3a269c634702d6c832564745fcb3048a8
DidacPH <magozero942.c1@gmail.com>
2017-09-22 11:38:32 +0200

    front&back
    Front:
    #Asossciacions.html(ligera modificacion)#
    #Shops.html deleted#
    Back:
    #functions(modificado texto de error)#
    #users(.html&php) hechos y restructurados#
    #users.js eliminado contenido, manteniendo archivo#

--- cpanel/inc/functions.php
+++ cpanel/inc/functions.php
@@ -3,7 +3,7 @@
 	{
 		$connexio=@mysqli_connect("localhost","root","","ddb99266");
 		if (!$connexio)
-		{	die("Error...".mysqli_connect_error());	}
+		{	die("Error al conectar");	}
 		mysqli_set_charset($connexio, "utf8");
 		return($connexio);
 	}
--- cpanel/models/users.php
+++ cpanel/models/users.php
@@ -4,30 +4,33 @@ require("../inc/functions.php");
 	if(isset($_GET['acc']) && $_GET['acc'] == 'login'){
 		$mySql = "SELECT idUser FROM users 
 					WHERE email='".$_GET['email']."' AND password='".$_GET['password'].
-					" AND active='Y'";
+					"' AND active='Y'";
 		$connexio = connect();
 		$resultLogin = mysqli_query($connexio, $mySql);
 		disconnect($connexio);
-
+		$checkLogin="0";
 		while ($row=mySqli_fetch_array($resultLogin))
 		{
 			
 			$checkLogin=$row['idUser'];
 		}
 	 	
-	 	//echo $checkLogin;
+
+	 	if($checkLogin == "0")
+	 	{
+	 		$mensaje = "Usuario y contraseña incorrectos";
+	 	}
+	 	else if($connexio == "Error al conectar")
+	 	{
+	 		$mensaje = "Error al conectar";
+	 	}
+	 	else
+	 	{
+	 		$mensaje = "Correcto";
+	 	}
+
+	 	echo $mensaje;
 
 	}
-/* 	else if (isset($_GET["acc"]) && $_GET["acc"] == "mail") {
- 		$mySql = "SELECT email FROM users WHERE idUser=".$_GET["idUser"];
-		$connexio = connect();
-		$resultContact = mysqli_query($connexio, $mySql);
-		disconnect($connexio);
-		while ($row=mySqli_fetch_array($resultContact))
-		{
-			
-			$dataContact=$row['email'];
-		}
-	 	echo $dataContact;
- 	}*/
+
  ?>
