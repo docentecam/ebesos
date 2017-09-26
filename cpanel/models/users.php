517ea4a6d3a096658230e04a226f9d9d1ccb6f02
DidacPH <magozero942.c1@gmail.com>
2017-09-26 11:42:48 +0200

    update associations&cpanel/users.php

--- cpanel/models/users.php
+++ cpanel/models/users.php
@@ -27,10 +27,47 @@ require("../inc/functions.php");
 	 	else
 	 	{
 	 		$mensaje = "Correcto";
+	 		$_SESSION['user'] = $checkLogin;
 	 	}
 
 	 	echo $mensaje;
 
 	}
+	else if (){
+		$mySql = "SELECT idUser FROM users 
+					WHERE email='".$_GET['email'];
+		$connexio = connect();
+		$resultCheck = mysqli_query($connexio, $mySql);
+		disconnect($connexio);
+		$checkEmail="0";
+		while ($row=mySqli_fetch_array($resultCheck))
+		{
+			
+			$checkEmail=$row['idUser'];
+		}
+	 	
 
- ?>
+	 	if($checkEmail == "0")
+	 	{
+	 		$mensaje = "L'usuari no existeix";
+	 	}
+	 	else if($connexio == "Error al conectar")
+	 	{
+	 		$mensaje = "Error al conectar";
+	 	}
+	 	else
+	 	{
+	 		$mySql = "UPDATE users 
+					SET pendentValidacio = 'true', validat = '$validat'
+					WHERE idOpinio = $idOpinio";
+			$connexio = connect();
+			$resultCheck = mysqli_query($connexio, $mySql);
+			disconnect($connexio);
+	 		$mensaje = "Correcto";
+	 	}
+
+	 	echo $mensaje;
+
+
+	}
+?>	
