27250fa43d9d5adca7f7ddb2f0731b4409dcc96b
SrUrn <ianeito95@gmail.com>
2017-10-04 10:09:16 +0200

    boton delete
    borra la tienda que sea. funcion comentada para evitar accidentes

--- cpanel/controllers/shops.js
+++ cpanel/controllers/shops.js
@@ -1,5 +1,8 @@
 angular.module('spaApp')
 .controller('ShopsCtrl', function($scope, $http, $routeParams) {
+	$scope.showShop = false;
+	$scope.shopOne = "[{{}}]";
+	console.log($scope.showShop);
 	$http({
 		method : "GET",
 		url : "models/shops.php?acc=list"
@@ -10,4 +13,35 @@ angular.module('spaApp')
 	}, function myError(response) {
 		$scope.shops = response.statusText;
 	});
+
+	$scope.shopOneEdit = function($idShop){
+		$http({
+			method : "GET",
+			url : "models/shops.php?acc=edit&idShop=" + $idShop
+		}).then(function mySucces (response) {
+			$scope.shopOne = response.data;
+			console.log($scope.shopOne);
+			console.log($scope.shopOne[0].name);
+			$scope.showShop = true;
+			console.log($scope.showShop);
+		}, function myError (response) {
+			$scope.showShop = response.statusText;
+		});
+	};
+	$scope.shopOneDelete = function($idShop){
+		console.log($idShop);
+		$http({
+			method : "GET",
+			url : "models/shops.php?acc=delete&idShop=" + $idShop
+		}).then(function mySucces (response) {
+			$scope.shopDeleted = response.data
+			//shopOneEdit($idShop);
+			console.log("hola: " + $scope.shopDeleted);
+		}, function myError (response) {
+			$scope.shopOne = response.statusText;
+		});
+	};
+	$scope.shopOneAdd = function($idShop){
+		$scope.showShop = true;
+	};
 });
--- cpanel/models/shops.php
+++ cpanel/models/shops.php
@@ -3,9 +3,10 @@ require('../inc/functions.php');
 
 		if(isset($_GET['acc']) && $_GET['acc'] == 'list')
 		{
-			$mySql= "SELECT si.url, s.name, s.description
+			$mySql = "SELECT si.url, s.idShop, s.name, s.description
 					FROM shopsimages si, shops s
-					WHERE si.preferred='Y'";
+					WHERE si.preferred='Y'
+					AND s.idShop = si.idShop";
 					
 
 			$connexio = connect();
@@ -22,12 +23,87 @@ require('../inc/functions.php');
 				{
 					$dataShops .= ",";
 				}
-				$dataShops .= '{"image":"'.$row['url'].'", "name":"'.$row['name'].'", "description":"'.$row['description'].'"}'; 
+				$dataShops .= '{"image":"'.$row['url'].'", "idShop":"'.$row['idShop'].'", "name":"'.$row['name'].'", "description":"'.$row['description'].'"}'; 
 				$i++;
 			}
 			$dataShops .= "]";
 
 			echo $dataShops;
 		}
+		else if(isset($_GET['acc']) && $_GET['acc'] == 'edit')
+		{
+			$idShop=$_GET['idShop'];
+			$mySql = "SELECT s.name, s.lng, s.lat, s.logo, s.telephone, s.email, s.address, s.schedule, s.description, s.descriptionLong, s.url AS web, s.cp, s.ciutat, si.url
+					FROM shops s, shopsimages si
+					WHERE si.preferred = 'Y'
+					AND s.idShop =".$idShop."
+					AND si.idShop =".$idShop;
 
+			$connexio = connect();
+
+			$resultShop = mysqli_query($connexio, $mySql);
+
+			disconnect($connexio);
+
+			$i = 0;
+			$dataShop = "[";
+			while($row = mysqli_fetch_array($resultShop))
+			{
+				if($i != 0) $dataShop .= ",";
+
+				$dataShop .= '{"name":"'.$row['name'].'", "lng":"'.$row['lng'].'", "lat":"'.$row['lat'].'", "logo":"'.$row['logo'].'", "telephone":"'.$row['telephone'].'", "email":"'.$row['email'].'", "address":"'.$row['address'].'", "schedule":"'.$row['schedule'].'", "description":"'.$row['description'].'", "descriptionLong":"'.$row['descriptionLong'].'", "web":"'.$row['web'].'", "cp":"'.$row['cp'].'", "ciutat":"'.$row['ciutat'].'", "imagePreferred":"'.$row['url'].'", "images":';
+				$j = 0;
+
+				$dataShop .= '[';
+				$mySql = "SELECT si.url
+				FROM shopsimages si
+				WHERE si.preferred = 'N'
+				AND si.idShop =".$idShop;
+
+				$connexio = connect();
+				$resultImgShop = mysqli_query($connexio, $mySql);
+				disconnect($connexio);
+
+				while($row = mysqli_fetch_array($resultImgShop))
+				{
+					if($j != 0) $dataShop .= ",";
+
+					$dataShop .= '{"image":"'.$row['url'].'"}';
+
+					$j++;
+				}
+				$dataShop .= ']}';
+				$i++;
+			}
+			$dataShop .= "]";
+
+			echo $dataShop;
+		}
+		else if(isset($_GET['acc']) && $_GET['acc'] == 'delete')
+		{
+			//TODO: Hay que borrar tambien los archivos fisicos
+			
+			$idShop=$_GET['idShop'];
+			$mySql = "DELETE FROM shopsimages WHERE idShop = $idShop";
+
+			$connexio = connect();
+
+			$deleteShop = mysqli_query($connexio, $mySql);
+
+			$mySql = "	DELETE FROM shops WHERE idShop = $idShop";
+
+			$connexio = connect();
+
+			$deleteShop = mysqli_query($connexio, $mySql);
+
+			disconnect($connexio);
+
+			// if($deleteShop)
+			// {
+			// 	echo "eliminado";
+			// }
+			// else echo "no eliminado";
+
+			echo $mySql;
+		}
 ?>
