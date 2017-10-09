bf03e6fd2d84f4e5c2f08fcac729f6ee321a7e8c
DidacPH <magozero942.c1@gmail.com>
2017-10-09 14:22:48 +0200

    lgin y cpanel/associations :8ball:

--- cpanel/controllers/users.js
+++ cpanel/controllers/users.js
@@ -5,11 +5,21 @@ angular.module('spaApp')
 				method : "GET",
 				url : "models/users.php?acc=loadUser&idUser=1" //modificar
 			}).then(function mySucces (response) {
-				$scope.names = response.data;
+				$scope.associations = response.data;
+				$scope.llega = "llega";
 			}, function myError (response) {
-				$scope.names = response.statusText;
+				$scope.associations = response.statusText;
 			});
 		
-
+			$scope.showChangePass = function(){
+  					$scope.changePass = true;
+				};
+			$scope.updateUser = function(){
+  					$scope.name = dataUser['name'].value;
+  					$scope.email = dataUser['email'].value;
+  					$scope.pswdMail = dataUser['pswdMail'].value;
+  					$scope.address = dataUser['address'].value;
+  					$scope.telephone = dataUser['telephone'].value;
+				};
 		
 	});
