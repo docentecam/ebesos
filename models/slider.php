a52209e0542de655d12feeef030f6d1eaef57f72
Nio94 <neofito94@gmail.com>
2017-10-18 15:04:42 +0200

    SPAM 🐟 THIS 🐟 TUNA 🐟 FOR 🐟 GOOD 🐟 FORTUNA
    Cuadro de texto del Slider añadido y con Bootstrap

--- models/slider.php
+++ models/slider.php
@@ -3,7 +3,7 @@ require("../inc/functions.php");
 	
 if (isset($_GET["acc"])&& ($_GET["acc"] == "imgSlider"))
 	{
-		$mySql = "SELECT image, link, description FROM slider";
+		$mySql = "SELECT image, link, description,title, subtitle FROM slider";
 		$connexio = connect();
 		$resultImgSlider = mysqli_query($connexio, $mySql);
 		disconnect($connexio);
@@ -15,7 +15,7 @@ if (isset($_GET["acc"])&& ($_GET["acc"] == "imgSlider"))
 			{
 				$dataImgSlider .= ",";				
 			}			
-			$dataImgSlider .= '{"image":"'.$row['image'].'","link":"'.$row['link'].'","description":"'.$row['description'].'"}';
+			$dataImgSlider .= '{"image":"'.$row['image'].'","link":"'.$row['link'].'","description":"'.$row['description'].'","title":"'.$row['title'].'","subtitle":"'.$row['subtitle'].'"}';
 			$i++;
 		}
 		$dataImgSlider .="]";
