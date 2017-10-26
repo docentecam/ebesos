a441474a0c53e424342ac9cdff59d72d294360a2
Nio94 <neofito94@gmail.com>
2017-10-26 10:16:35 +0200

    cambios svg

--- cpanel/controllers/users.js
+++ cpanel/controllers/users.js
@@ -193,6 +193,10 @@ angular.module('spaApp')
   				$scope.logoEdit2 = true;
   			};
 
-  				
+
+			$scope.showDisconnect = function(){
+
+			$scope.showSignOff=true;
+	}  				
 			
 	});
--- cpanel/main.php
+++ cpanel/main.php
@@ -43,13 +43,13 @@ if(!isset($_SESSION['idUser'])) header("Location: index.html");
 			<div class="col-lg-3 pull-right">
 				<div class="row">
 					<div class="col-xs-2 col-lg-4 col-lg-push-7" style="margin-top:5%;">
-						<a href="" id="deleteUnderlineSignUp">
-							<i class="fa fa-user-circle-o fa-2x colorLoginHidden" aria-hidden="true"  style=": #52A4B3" title="Inicia Sessió"></i>
+						<a href="" id="deleteUnderlineSignUp" ng-click="showDisconnect">
+							<i class="fa fa-user-circle-o fa-2x colorLoginHidden" aria-hidden="true" title="Inicia Sessió"></i>
 							<i class="fa fa-caret-down fa-1x" style="margin-left: 10px;" aria-hidden="true" ng></i>
 						</a>						
 					</div>	
 				</div>
-				<div class="row">
+				<div class="row" ng-show="showLogOff">
 					<div class="col-xs-2 col-lg-6 col-lg-push-4" style="background-color: #D5D5D5; position: absolute; top: 120%; right: 0%; height: 40px; text-align: center; padding-top: 8.4px; border-top: 3px solid #7CA3E3">
 						<i class="fa fa-sign-out" aria-hidden="true"></i>
 						<a href="models/users.php?acc=logout">Tancar Sessió</a>
