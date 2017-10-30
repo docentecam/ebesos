dc428110b69b044b1eebe3d5702df9eca17d5306
username(Jordi) <email>
2017-10-30 12:32:46 +0100

    BOOTSRAP LG CONTACT

--- controllers/users.js
+++ controllers/users.js
@@ -16,18 +16,19 @@ angular.module('spaApp')
 
 angular.module('spaApp')															 
 	.controller('ContactCtrlUser', function($scope, $http ,$routeParams) {
-	$scope.idUser = $routeParams.idUser;
+		// $scope.loading=true;
+		$scope.idUser = $routeParams.idUser;
 
-	var a = Math.ceil(Math.random() * 9)+ '';
-	var b = Math.ceil(Math.random() * 9)+ '';
-	var c = Math.ceil(Math.random() * 9)+ '';
-	var d = Math.ceil(Math.random() * 9)+ '';
-	var e = Math.ceil(Math.random() * 9)+ '';
-	var code = a + b + c + d + e;
-	document.getElementById("txtCaptcha").value = code;
-	document.getElementById("captchaDiv").innerHTML = code;
+		var a = Math.ceil(Math.random() * 9)+ '';
+		var b = Math.ceil(Math.random() * 9)+ '';
+		var c = Math.ceil(Math.random() * 9)+ '';
+		var d = Math.ceil(Math.random() * 9)+ '';
+		var e = Math.ceil(Math.random() * 9)+ '';
+		var code = a + b + c + d + e;
+		document.getElementById("txtCaptcha").value = code;
+		document.getElementById("captchaDiv").innerHTML = code;
 
-	$http({
+		$http({
 			method : "GET",
 			url : "models/users.php?acc=infoMail&idUser="+ $scope.idUser
 		}).then(function mySucces (response) {
@@ -36,6 +37,9 @@ angular.module('spaApp')
 		}, function myError (response) {
 			$scope.infoMail = response.statusText;
 		});
+		// .finally(function() {
+		// $scope.loading=false;
+		// });	
 
 		$scope.checkform = function()
 		{
@@ -73,22 +77,22 @@ angular.module('spaApp')
 				});
 			}
 		}
-	$scope.validCaptcha = function()
-	{
-		var str1 = $scope.removeSpaces(myForm['txtCaptcha'].value);
-		var str2 = $scope.removeSpaces(myForm['captchaInput'].value);
-		if (str1 == str2){
-			return true;
+		$scope.validCaptcha = function()
+		{
+			var str1 = $scope.removeSpaces(myForm['txtCaptcha'].value);
+			var str2 = $scope.removeSpaces(myForm['captchaInput'].value);
+			if (str1 == str2){
+				return true;
+			}
+			else{
+				return false;
+			}
 		}
-		else{
-			return false;
+		$scope.removeSpaces = function(stringCaptcha){
+			return stringCaptcha.split(' ').join('');
 		}
-	}
-	$scope.removeSpaces = function(stringCaptcha){
-		return stringCaptcha.split(' ').join('');
-	}
 
-});
+	});
 
 angular.module('spaApp')
 
