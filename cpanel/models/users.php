7ac98e29a49ec86e4a21821c1b3c8badc4567836
DidacPH <magozero942.c1@gmail.com>
2017-11-03 12:48:26 +0100

    IS NULL

--- cpanel/models/users.php
+++ cpanel/models/users.php
@@ -165,7 +165,7 @@ session_start();
 		{
 
 			$mySql = "UPDATE users
-					SET password='".sha1(md5($_GET['password']))."', forgotToken=NULL 
+					SET password='".sha1(md5($_GET['password']))."', forgotToken IS NULL 
 					WHERE forgotToken='".$_GET['fToken']."'";
 			$connexio = connect();
 			$updatePass = mysqli_query($connexio, $mySql);
