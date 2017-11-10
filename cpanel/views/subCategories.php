f423fdaf4ab2cd64201836a7a9ad6c5a26657b21
username(Jordi) <email>
2017-11-10 13:55:15 +0100

    GG WP

--- cpanel/views/subCategories.php
+++ cpanel/views/subCategories.php
@@ -3,7 +3,7 @@
 	if(!isset($_SESSION['user']['idUser'])) header("Location: index.html");
 ?>
 <div class="row" id="topDiv1">
-	<label class="col-lg-1 col-xs-12">Subcategories</label>
+	<label class="col-lg-1 col-xs-12 labelSubCategoria">Subcategories</label>
 </div>
 <div class="row">
 	<span class="col-lg-1 titleCategoria">Categoria:</span>
@@ -17,12 +17,13 @@
 <div class="row tableSubCategories">
 	<div class="col-lg-8 borderTableSubCategories noPadding">
 		<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-			<div class="col-lg-12 tableCategoriaSubCategoria">
+			<div class="col-lg-12 tableCategoriaSubCategoria noPadding">
 				CATEGORIA
 			</div>
 		</div>
+		<img class="iconSubCategory" width="50px" height="50px" src="../img/burger.svg" alt="" >
 		<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-			<div class="col-lg-12 tableAlimentacioSubCategoria">
+			<div class="col-lg-12 tableAlimentacioSubCategoria noPadding">
 				ALIMENTACIÓ
 			</div>
 		</div>
