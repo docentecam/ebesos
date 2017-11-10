d6923338e65c8056897359136fd18869ff95da38
DidacPH <magozero942.c1@gmail.com>
2017-11-10 10:32:08 +0100

    cpanel/links
    todo menos CSS

--- cpanel/views/links.php
+++ cpanel/views/links.php
@@ -2,28 +2,43 @@
 session_start();
 if(!isset($_SESSION['user']['idUser'])) header("Location: index.html");
 ?>
-
 <div class="row">
-	<button id="btnAfegir" class="btn btn-default" ng-hide="btnAfegir" ng-click="" >Afegir <i class="fa fa-plus-circle"></i></button>
-	<br>
-	<table border="1">
-		<thead>
-		<tr>
-			<th>Literal</th>
-			<th>Value</th>
-			<th>Editar</th>
-			<th>Eliminar</th>
-		</tr>
-	</thead>
-
-	<tbody>
-		<tr ng-repeat="link in linksList">
-			<td>{{link.description}}</td>
-			<td>{{link.url}}</td>
-			<td><a ng-click="" href=""><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
-			<td><a ng-click="" href=""><i class="fa fa-times" aria-hidden="true"></i></a></td>	
-		</tr>
-	</tbody>
-	</table>
-
+	<div class="col-lg-3"><b>Enllaços peu de pàgina</b></div>
+	<div class="col-lg-5" ng-show="tableLinks">
+		<button id="btnAfegir" class="btn btn-default" ng-hide="btnAfegir" ng-click="createLink()">Afegir <i class="fa fa-plus-circle"></i></button>
+	</div>
+</div>
+<div ng-show="tableLinks">
+	<div class="row">
+		<table border="1">
+			<thead>
+			<tr>
+				<th>Literal</th>
+				<th>Value</th>
+				<th>Editar</th>
+				<th>Eliminar</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr ng-repeat="link in linksList">
+				<td>{{link.description}}</td>
+				<td>{{link.url}}</td>
+				<td><a ng-click="editLink(link.idLink,link.description,link.url)" href=""><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
+				<td><a ng-click="deleteLink(link.idLink)" href=""><i class="fa fa-times" aria-hidden="true"></i></a></td>	
+			</tr>
+		</tbody>
+		</table>
+	</div>
+</div>
+<div ng-show="formLinks">
+	<div class="row">
+		<form id="linksForm" ng-submit="updateLink()">
+			<input type="text" id="userId" ng-value="idLinkC" disabled hidden>
+			<label class="col-lg-3 padd">Nom: </label>
+			<div class="col-lg-9 padd"><input type="text" name="name" ng-model="nameC" style="width: 40%;"></div>
+			<label class="col-lg-3 padd">Url: </label>
+			<div class="col-lg-9 padd"><input type="text" name="url" ng-model="urlC" style="width: 40%;"></div>
+			<div class="col-lg-2 padd"><input class="btn btn-default" type="submit" value="Confirmar"></div>
+		</form>		
+	</div>
 </div>
