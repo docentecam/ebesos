f1f949a50c83ff96f4e56c673d6d925d3e210d90
DidacPH <magozero942.c1@gmail.com>
2017-11-13 11:17:47 +0100

    cpanel/subcategories
    hecha la funcionalidad (falta por corregir un error que solo permite
    editar y eliminar una vez)

--- cpanel/controllers/subCategories.js
+++ cpanel/controllers/subCategories.js
@@ -2,21 +2,206 @@ angular.module('spaApp')
 .controller('SubcategoriesCtrl', function($scope, $http) {
 	$scope.loading=true;
 	$scope.firstC = true;
+	$scope.subCatTable = true;
 	$http({
 			method : "GET",
 			url : "models/subCategories.php?acc=l"
 		}).then(function mySucces (response) {
 			$scope.categories = response.data;
+			$scope.urlPicto1C = $scope.categories[0]['urlPicto1'];
 		}, function myError (response) {
 			$scope.categories = response.statusText;
 		})
 		.finally(function() {
 			$scope.loading=false;
+	});
+	$scope.loading=true;
+	$http({
+			method : "GET",
+			url : "models/subCategories.php?acc=ls"
+		}).then(function mySucces (response) {
+			$scope.subCategories = response.data;
+		}, function myError (response) {
+			$scope.subCategories = response.statusText;
+		})
+		.finally(function() {
+			$scope.loading=false;
+	});
+	$scope.changeCat = function(name){
+		$scope.firstC = false;
+		$scope.nameC = name;
+		$scope.loading=true;
+		$http({
+				method : "GET",
+				url : "models/subCategories.php?acc=l&name="+name
+			}).then(function mySucces (response) {
+				$scope.uCategories = response.data;
+				$scope.urlPicto1C = $scope.uCategories[0]['urlPicto1'];
+				$scope.idCategoryC = $scope.uCategories[0]['idCategory'];
+				$scope.loading=true;
+				$http({
+						method : "GET",
+						url : "models/subCategories.php?acc=ls&idCategory="+$scope.idCategoryC
+					}).then(function mySucces (response) {
+						$scope.subCategories = response.data;
+					}, function myError (response) {
+						$scope.subCategories = response.statusText;
+					})
+					.finally(function() {
+						$scope.loading=false;
+				});
+			}, function myError (response) {
+				$scope.uCategories = response.statusText;
+			})
+			.finally(function() {
+				$scope.loading=false;
 		});
-		$scope.changeCat = function(idCategory,name,urlPicto1){
-			$scope.firstC = false;
-			$scope.idCategoryC = idCategory;
-			$scope.nameC = name;
-			$scope.urlPicto1C = urlPicto1;
+	}
+	$scope.addSubCat = function(action){
+		$scope.subCatTable = false;
+		$scope.btnName = true;
+		$scope.nameSC = "";
+		$scope.accA = action;
+	}
+	$scope.editSubCat = function(idSubCategory,name,action){
+		$scope.subCatTable = false;
+		$scope.btnName = false;
+		$scope.nameSC = name;
+		$scope.idSubCategorySC = idSubCategory;
+		$scope.accA = action;
+	}
+	$scope.addBBDDSubCat = function(){
+		$scope.idC = subCategoresAE['CategoryName'].value;
+		$scope.idSCName = subCategoresAE['nsc'].value;
+		$scope.action = subCategoresAE['addAcc'].value;
+		$scope.idSC = subCategoresAE['idSubC'].value;
+
+		if($scope.idC == -1 || $scope.idSCName == "")
+		{
+			alert("Camp buit");
 		}
+		else if($scope.action == "a")
+		{
+			$http({
+				method : "GET",
+				url : "models/subCategories.php?acc=c&idCategory="+$scope.idC+"&name="+$scope.idSCName
+			}).then(function mySucces (response) {
+				$scope.createSubCat = response.data;
+				$scope.loading=true;
+				$http({
+					method : "GET",
+					url : "models/subCategories.php?acc=l"
+				}).then(function mySucces (response) {
+					$scope.categories = response.data;
+					$scope.urlPicto1C = $scope.categories[0]['urlPicto1'];
+					$scope.nameC = $scope.categories[0]['name'];
+					$scope.firstC = false;
+				}, function myError (response) {
+					$scope.categories = response.statusText;
+				})
+				.finally(function() {
+					$scope.loading=false;
+				});
+				$scope.loading=true;
+				$http({
+						method : "GET",
+						url : "models/subCategories.php?acc=ls"
+					}).then(function mySucces (response) {
+						$scope.subCategories = response.data;
+						$scope.subCatTable = true;
+					}, function myError (response) {
+						$scope.subCategories = response.statusText;
+					})
+					.finally(function() {
+						$scope.loading=false;
+				});
+			}, function myError (response) {
+				$scope.createSubCat = response.statusText;
+			})
+			.finally(function() {
+				$scope.loading=false;
+			});
+		}
+		else
+		{
+			$http({
+				method : "GET",
+				url : "models/subCategories.php?acc=e&idCategory="+$scope.idC+"&name="+$scope.idSCName+"&idSubCategory="+$scope.idSC
+			}).then(function mySucces (response) {
+				$scope.editSubCat = response.data;
+				$http({
+					method : "GET",
+					url : "models/subCategories.php?acc=l"
+				}).then(function mySucces (response) {
+					$scope.categories = response.data;
+					$scope.urlPicto1C = $scope.categories[0]['urlPicto1'];
+					$scope.nameC = $scope.categories[0]['name'];
+					$scope.firstC = false;
+				}, function myError (response) {
+					$scope.categories = response.statusText;
+				})
+				.finally(function() {
+					$scope.loading=false;
+				});
+				$scope.loading=true;
+				$http({
+						method : "GET",
+						url : "models/subCategories.php?acc=ls"
+					}).then(function mySucces (response) {
+						$scope.subCategories = response.data;
+						$scope.subCatTable = true;
+					}, function myError (response) {
+						$scope.subCategories = response.statusText;
+					})
+					.finally(function() {
+						$scope.loading=false;
+				});
+			}, function myError (response) {
+				$scope.editSubCat = response.statusText;
+			})
+			.finally(function() {
+				$scope.loading=false;
+			});
+		}
+	}
+	$scope.deleteSubCat = function(idSubCategory){
+			$http({
+				method : "GET",
+				url : "models/subCategories.php?acc=d&idSubCategory="+idSubCategory
+			}).then(function mySucces (response) {
+				$scope.deleteSubCat = response.data;
+				$http({
+					method : "GET",
+					url : "models/subCategories.php?acc=l"
+				}).then(function mySucces (response) {
+					$scope.categories = response.data;
+					$scope.urlPicto1C = $scope.categories[0]['urlPicto1'];
+					$scope.nameC = $scope.categories[0]['name'];
+					$scope.firstC = false;
+				}, function myError (response) {
+					$scope.categories = response.statusText;
+				})
+				.finally(function() {
+					$scope.loading=false;
+				});
+				$scope.loading=true;
+				$http({
+						method : "GET",
+						url : "models/subCategories.php?acc=ls"
+					}).then(function mySucces (response) {
+						$scope.subCategories = response.data;
+						$scope.subCatTable = true;
+					}, function myError (response) {
+						$scope.subCategories = response.statusText;
+					})
+					.finally(function() {
+						$scope.loading=false;
+				});
+			}, function myError (response) {
+				$scope.deleteSubCat = response.statusText;
+			})
+			.finally(function() {
+				$scope.loading=false;
+			});
+	}
 });
--- cpanel/views/subCategories.php
+++ cpanel/views/subCategories.php
@@ -5,50 +5,62 @@
 <div class="row">
 	<label class="col-lg-1 col-xs-12">Subcategories</label>
 </div>
-<div class="row">
-	<span class="col-lg-1 titleCategoria">Categoria:</span>
-	<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories" ng-change="changeCat(idCategory,name,urlPicto1)" ng-model="idCategory">
-		<option ng-repeat="category in categories" ng-value="category.name" ng-selected="category.idCategory==1">{{category.name}}</option>
-	</select>
-</div>
-<div class="row tableSubCategories">
-	<div class="col-lg-8 borderTableSubCategories noPadding">
-		<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-			<div class="col-lg-12 tableCategoriaSubCategoria noPadding">
-				CATEGORIA
+<div ng-show="subCatTable">
+	<div class="row">
+		<span class="col-lg-1 titleCategoria">Categoria:</span>
+		<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories" ng-change="changeCat(idCategory)" ng-model="idCategory">
+			<option ng-repeat="category in categories" ng-value="category.name" ng-selected="category.idCategory==1">{{category.name}}</option>
+		</select>
+	</div>
+	<div class="row tableSubCategories">
+		<div class="col-lg-8 borderTableSubCategories noPadding">
+			<div class="col-lg-6 borderTitlesSubCategories centerPadding">
+				<div class="col-lg-12 tableCategoriaSubCategoria noPadding">
+					CATEGORIA
+				</div>
+			</div>
+			<img class="iconSubCategory" width="50px" height="50px" src="../img/pictograms/{{urlPicto1C}}" alt="" >
+			<div class="col-lg-6 borderTitlesSubCategories centerPadding">
+				<div class="col-lg-12 tableAlimentacioSubCategoria">
+					<div ng-show="firstC">ALIMENTACIÓ</div>{{nameC | uppercase}}
+				</div>
+			</div>
+			<div ng-repeat="subCategory in subCategories">
+				<div class="col-lg-8 borderContentSubCategories">
+					<div class="col-lg-12 centerPadding">
+						{{subCategory.name+"-"+subCategory.idSubCategory}}
+					</div>
+				</div>
+				<button class="col-lg-2 btn btn-SubCategory borderBtnEditSubCategories" id="editTable" ng-click="editSubCat(subCategory.idSubCategory,subCategory.name,'e')">
+					Editar
+				</button>
+				<button class="col-lg-2 btn btn-SubCategory borderBtnDelSubCategories" id="deleteTable" ng-click="deleteSubCat(subCategory.idSubCategory)">
+					Eliminar
+				</button>
 			</div>
 		</div>
-		<img class="iconSubCategory" width="50px" height="50px" src="../img/burger.svg" alt="" >
-		<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-			<div class="col-lg-12 tableAlimentacioSubCategoria">
-				<div ng-show="firstC">ALIMENTACIÓ</div>{{idCategoryC | uppercase}}
-			</div>
-		</div>
-		<div class="col-lg-8 borderContentSubCategories">
-			<div class="col-lg-12 centerPadding">
-				Aviram
-			</div>
-		</div>
-		<button class="col-lg-2 btn btn-SubCategory borderBtnEditSubCategories">
-			Editar
-		</button>
-		<button class="col-lg-2 btn btn-SubCategory borderBtnDelSubCategories">
-			Eliminar
-		</button>
+	</div>
+	<div class="row">
+		<button id="btnAfegirSubCategoria" class="btn btn-default" ng-click="addSubCat('a')">Afegir <i class="fa fa-plus-circle"></i></button>
 	</div>
 </div>
-<div class="row">
-	<button id="btnAfegirSubCategoria" class="btn btn-default">Afegir <i class="fa fa-plus-circle"></i></button>
+<div ng-hide="subCatTable">
+	<form id="subCategoresAE">
+		<div class="row">
+			<span class="titleCategoria col-lg-1">Escull categoria:</span>
+			<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories" id="CategoryName">
+				<option value="-1">Categoria</option>
+				<option ng-repeat="category in categories" ng-value="category.idCategory">{{category.name}}</option>
+			</select>
+		</div>
+		<div>
+			<input type="text" class="col-lg-3 inputTextSubCategories" id="idSubC" ng-model="idSubCategorySC" hidden disabled>
+			<input type="text" class="col-lg-3 inputTextSubCategories" id="addAcc" ng-model="accA" hidden disabled>
+		</div>
+		<div class="row">
+			<span class="titleCategoria col-lg-1">Nom subcategoria:</span>
+			<input type="text" class="col-lg-3 inputTextSubCategories" ng-model="nameSC" id="nsc">
+		</div>
+		<button id="afegirSubCategoria" class="btn btn-default centerPadding" ng-click="addBBDDSubCat()"><span ng-show="btnName">Afegir</span><span ng-hide="btnName">Editar</span></button>
+	</form>
 </div>
-<div class="row">
-	<span class="titleCategoria col-lg-1">Escull categoria:</span>
-	<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories">
-		<option value="-1">Categoria</option>
-		<option value="alimentacio">Alimentació</option>
-	</select>
-</div>
-<div class="row">
-	<span class="titleCategoria col-lg-1">Nom subcategoria:</span>
-	<input type="text" class="col-lg-3 inputTextSubCategories">
-</div>
-<button id="afegirSubCategoria" class="btn btn-default centerPadding">Afegir</button> 
