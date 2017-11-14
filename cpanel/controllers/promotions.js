8adc462d2cb3f581f41ee536c2fd06621bc82906
lander08 <lander.sanahujes@gmail.com>
2017-11-14 12:20:08 +0100

    promotions
    wabadadu

--- cpanel/controllers/promotions.js
+++ cpanel/controllers/promotions.js
@@ -6,16 +6,76 @@ angular.module('spaApp')
  		$scope.loadPromotions=true;
 
 		$scope.act="";
+		
+		$scope.accBbdd="";
 
- 		$scope.promotions=[];
- 		$scope.promotions.image="";
+ 		$scope.promotion={};
+ 		$scope.promotion.image="";
+ 		$scope.promotion.conditionsVals="";
+ 		$scope.promotion.dateExpireVals="";
+ 		$scope.promotion.dateExpireEix="";
+ 		$scope.promotion.oferVals="";
+ 		$scope.promotion.conditionsEix="";
+ 		$scope.promotion.oferEix="";
+ 		
+
+ 		var d = new Date();
+		var yyyy = d.getFullYear();
+		var mm = d.getMonth() < 9 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1); // getMonth() is zero-based
+		var dd  = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
+		$scope.promotion.dateExpireVals =yyyy+"-"+mm+"-"+dd;
+
+		$scope.promotion.dateExpireEix = yyyy+"-"+mm+"-"+dd;
+
 
  			$scope.loading=true;
 			$http({
 				method : "GET",
 				url : "models/promotions.php?acc=l"
 			}).then(function mySucces (response) {
-				$scope.promotionsList = response.data;
+				$scope.shopsList = response.data[0].dataShops;
+				$scope.promotionsList=response.data[0].dataPromotions;
+				
+			}, function myError (response) {
+				$scope.promotionsList = response.statusText;
+			})
+			.finally(function() 
+		{ 
+		    $scope.loading=false; 
+		})
+
+			
+
+
+	$scope.dataPromotion = function(act="",idPromotion)
+	{
+		console.log(idPromotion);
+		$scope.loadPromotions=false;
+ 		
+		if (act=='Editar')
+		{
+
+			$scope.act=act;
+			$scope.showPromotions=true;
+			$scope.accBbdd="Update";
+
+
+			$http({
+				method : "GET",
+				url : "models/promotions.php?acc=l&idPromotion="+idPromotion
+			}).then(function mySucces (response) {
+				$scope.promotionSel = response.data[0].dataPromotions;
+				console.log($scope.promotionSel);
+				$scope.promotion.idPromotion=$scope.promotionSel[0].idPromotion;
+				$scope.promotion.image=$scope.promotionSel[0].image;
+				$scope.promotion.conditionsVals=$scope.promotionSel[0].conditionsVals;
+				$scope.promotion.dateExpireVals=$scope.promotionSel[0].dateExpireVals;
+				$scope.promotion.dateExpireEix=$scope.promotionSel[0].dateExpireEix;
+				$scope.promotion.oferVals=$scope.promotionSel[0].oferVals;
+				$scope.promotion.conditionsEix=$scope.promotionSel[0].conditionsEix;
+				$scope.promotion.oferEix=$scope.promotionSel[0].oferEix;
+				$scope.promotion.shopSelected=$scope.promotionSel[0].idShop;
+				console.log($scope.promotionSel[0].conditionsVals);
 
 				
 			}, function myError (response) {
@@ -27,16 +87,6 @@ angular.module('spaApp')
 		})
 
 
-	$scope.editPromotions = function(act="",idPromotion)
-	{
-
-		$scope.loadPromotions=false;
- 		
-	
-		if (act=='Editar')
-		{
-			$scope.act=act;
-			$scope.showPromotions=true;
 			
 		}
 
@@ -46,6 +96,34 @@ angular.module('spaApp')
 			$scope.showPromotions=true;
 			
 		}
+	}
+
+	$scope.editPromotion=function()
+	{
+		console.log($scope.promotion);
+		var data = new FormData();
+		data.append("idPromotion", $scope.idPromotion);
+		data.append("image", $scope.image);
+		data.append("conditionsVals", $scope.conditionsVals);
+		data.append("dateExpireVals", $scope.dateExpireVals);
+		data.append("dateExpireEix", $scope.dateExpireEix);
+		data.append("oferVals", $scope.oferVals);
+		data.append("conditionsEix", $scope.conditionsEix);
+		data.append("oferEix", $scope.oferEix);
+		data.append("shopSelected", $scope.shopSelected);
+		var deferred=$q.defer();
+			return $http.post("models/promotions.php?acc=updatePromotion", data,{
+				headers:{
+					"Content-type":undefined
+				},
+				transformRequest:angular.identity
+			})
+			.then(function(res)
+				{
+					
+					deferred.resolve(res);
+					
+				})
 	}
 
 });
--- cpanel/views/promotions.php
+++ cpanel/views/promotions.php
@@ -6,25 +6,22 @@
 
 	<button id="btnAdd" class="btn btn-default" ng-click="editPromotions('Afegir')">Afegir <i class="fa fa-plus-circle"></i></button>
 
-	<div class="row" ng-repeat="promotion in promotionsList | filter:{active: 'N'}">		
+	<div class="row" ng-repeat="promotionList in promotionsList | filter:{active: 'N'}">		
 		<div class="col-lg-3">
-			<img class="img-responsive" ng-src="../img/promotions/{{promotion.image}}">	
+			<img class="img-responsive" ng-src="../img/promotions/{{promotionList.image}}">	
 		</div>
 		
-		<div>
-			{{promotion.creation}}
-		</div>
 		<div class="col-lg-5">
-			{{promotion.conditionsVals}}<br>
-			<label>Pendent De Aprovació</label> &nbsp;&nbsp;&nbsp;<input type="button" class="btn btn-success" value="Actualitzar"  class="btn btn-default">
+			{{promotionList.conditionsVals}}<br>
+			<label>Pendent De Aprovació</label> &nbsp;&nbsp;&nbsp;<input type="button" class="btn btn-success" value="Activar"  class="btn btn-default">
 		</div>
 		<div class="col-lg-2">
-			{{promotion.dateExpireVals}}
+			Data Caducitat Vals {{promotionList.dateExpireVals}}
 			<br>
-			{{promotion.dateExpireEix}}
+			Data Caducitat Eix{{promotionList.dateExpireEix}} 
 		</div>
 		<div class="col-lg-1">
-			<button id="" class="btn btn-default" ng-click="editPromotions('Editar')">Editar <i class="fa fa-pencil" aria-hidden="true"></i></button>
+			<button id="" class="btn btn-default" ng-click="editPromotions('Editar' , promotionList.idPromotion)">Editar <i class="fa fa-pencil" aria-hidden="true"></i></button>
 
 		</div>
 		<div class="col-lg-1">
@@ -32,24 +29,25 @@
 		</div>
 	</div>
 <hr>
-	<div class="row" ng-repeat="promotion in promotionsList | filter:{active: 'Y'}">		
+	<div class="row" ng-repeat="promotionList in promotionsList | filter:{active: 'Y'}">		
 		<div class="col-lg-3">
-			<img class="img-responsive" ng-src="../img/promotions/{{promotion.image}}">	
+			<img class="img-responsive" ng-src="../img/promotions/{{promotionList.image}}">	
 		</div>
 		
 		<div>
-			{{promotion.creation}}
+			{{promotionList.creation}}
 		</div>
 		<div class="col-lg-5">
-			{{promotion.conditionsVals}}
+			{{promotionList.conditionsVals}}
+			{{promotionList.conditionsEix}}
 		</div>
 		<div class="col-lg-2">
-			{{promotion.dateExpireVals}}
+			Data Caducitat Vals{{promotionList.dateExpireVals}}
 			<br>
-			{{promotion.dateExpireEix}}
+			 Data Caducitat Eix{{promotionList.dateExpireEix}}
 		</div>
 		<div class="col-lg-1">
-			<button id="" class="btn btn-default" ng-click="editPromotions('Editar')">Editar <i class="fa fa-pencil" aria-hidden="true"></i></button>
+			<button id="" class="btn btn-default" ng-click="dataPromotion('Editar', promotionList.idPromotion)">Editar <i class="fa fa-pencil" aria-hidden="true"></i></button>
 
 		</div>
 		<div class="col-lg-1">
@@ -61,48 +59,57 @@
 
 <div class="row" ng-show="showPromotions">
 	<form action="#" id="validationPromotion" name="validation" method="POST" >
-		<h1>{{act}} Promotions</h1>
+		<h1>{{act}} Promocions</h1>
+		<label>Seleccionar comerç </label> <select name="listShops" ng-model="promotion.shopSelected" > <option ng-repeat="shop in shopsList" ng-selected="promotion.shopSelected==shop.idShop">{{shop.name}}</option> </select>
+		<br><br>
+		<label>Imatge: </label>  <input type="file" name="">
+
 	<div class="row">
 		<div class="col-lg-6">
-			<div>
-				<label>Titol </label>
-				<input type="text" id="">
-			</div>
-			<div>
-				<label>Actualitzada data de pujada </label>
-				<input type="date"  id="">
-			</div>
-			<div>
-				<label>Seleccionar comerç </label>
-				<select></select>
-			</div>
-			<div>
-				<label>Condicions del Val </label>
-				<textarea></textarea>
-			</div>
+			<fieldset>
+			 <legend> VALS </legend>
+				<div>
+					<label>OFERTA </label>
+					<textarea>{{promotion.oferVals}}</textarea>
+				</div>
+				<div>
+					<label>Data Expire Vals </label>
+					<input type="date" ng-model="promotion.dateExpireVals" id="">
+				</div>
+				
+				<div>
+					<label>Condicions del Val </label>
+					<textarea>{{promotion.conditionsVals}}</textarea>
+				</div>
+			</fieldset>
 		</div>
+
 	</div>
 
 	<div class="row">
 		<div class="col-lg-6">
+			<h1>Eix</h1>
+			
 			<div>
-				<label>Creació </label>
-				<input type="date"  id="">
+				<label>OFERTA </label>
+			<textarea>{{promotion.oferEix}}</textarea>
 			</div>
+
 			<div>
-				<label>Data de caducitat del Val </label>
-				<input type="date" id="">
+				<label>Data Expire Eix </label>
+				<input type="date" ng-model="promotion.dateExpireEix" id="">
 			</div>
-			<div>
-				<label>Imatge </label>
-				<input type="file" name="">
-			</div>
+
 			<div>
 				<label>Condicions del Val </label>
-				<input type="text" name="">
+				<textarea>{{promotion.conditionsEix}}</textarea>
 			</div>
 		</div>
 	</div>
+	<div>
+		<input type="submit" class="btn btn-default" value="Guardar dades" ng-click="editPromotion()">
+	</div>
+	
 	</form>
 </div>
 
