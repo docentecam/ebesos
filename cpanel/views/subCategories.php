a122678a8e0f1f254c082c7d1b5e4f7cce3d10ef
username(Jordi) <email>
2017-11-14 13:13:10 +0100

    No os cargueis el subcategorias, gracias c:

--- cpanel/views/subCategories.php
+++ cpanel/views/subCategories.php
@@ -3,38 +3,38 @@
 	if(!isset($_SESSION['user']['idUser'])) header("Location: index.html");
 ?>
 <div class="row">
-	<label class="col-xs-1 col-xs-12">Subcategories</label>
+	<label class="col-lg-1 col-xs-12">Subcategories</label>
 </div>
 <div ng-show="subCatTable">
 	<div class="row">
-		<span class="col-lg-1 titleCategoria">Categoria:</span>
-		<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories" ng-change="changeCat(idCategory)" ng-model="idCategory">
+		<span class="col-xs-4 col-sm-3 col-md-2 titleCategoria">Categoria:</span>
+		<select name="subCategoriesSelect" class="col-xs-3 selectSubCategories" ng-change="changeCat(idCategory)" ng-model="idCategory">
 			<option ng-repeat="category in categories" ng-value="category.name" ng-selected="category.idCategory==1">{{category.name}}</option>
 		</select>
 	</div>
 	<div class="row tableSubCategories">
-		<div class="col-lg-8 borderTableSubCategories noPadding">
-			<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-				<div class="col-lg-12 tableCategoriaSubCategoria noPadding">
+		<div class="col-xs-11 col-lg-8 borderTableSubCategories noPadding">
+			<div class="col-xs-6 borderTitlesSubCategories centerPadding">
+				<div class="col-xs-12 tableCategoriaSubCategoria noPadding">
 					CATEGORIA
 				</div>
 			</div>
 			<img class="iconSubCategory" width="50px" height="50px" src="../img/pictograms/{{urlPicto1C}}" alt="" >
-			<div class="col-lg-6 borderTitlesSubCategories centerPadding">
-				<div class="col-lg-12 tableAlimentacioSubCategoria">
+			<div class="col-xs-6 borderTitlesSubCategories centerPadding">
+				<div class="col-xs-12 tableAlimentacioSubCategoria">
 					<div ng-show="firstC">ALIMENTACIÓ</div>{{nameC | uppercase}}
 				</div>
 			</div>
 			<div ng-repeat="subCategory in subCategories">
-				<div class="col-lg-8 borderContentSubCategories">
-					<div class="col-lg-12 centerPadding">
+				<div class="col-xs-8 borderContentSubCategories">
+					<div class="col-xs-12 centerPadding">
 						{{subCategory.name}}
 					</div>
 				</div>
-				<button class="col-lg-2 btn btn-SubCategory borderBtnEditSubCategories" id="editTable" ng-click="editSubCat(subCategory.idSubCategory,subCategory.name,'e')">
+				<button class="col-xs-2 btn btn-SubCategory borderBtnEditSubCategories" id="editTable" ng-click="editSubCat(subCategory.idSubCategory,subCategory.name,'e')">
 					Editar
 				</button>
-				<button class="col-lg-2 btn btn-SubCategory borderBtnDelSubCategories" id="deleteTable" ng-click="deleteSubCat(subCategory.idSubCategory)">
+				<button class="col-xs-2 btn btn-SubCategory borderBtnDelSubCategories" id="deleteTable" ng-click="deleteSubCat(subCategory.idSubCategory)">
 					Eliminar
 				</button>
 			</div>
@@ -47,19 +47,19 @@
 <div ng-hide="subCatTable">
 	<form id="subCategoresAE">
 		<div class="row">
-			<span class="titleCategoria col-lg-1">Escull categoria:</span>
-			<select name="subCategoriesSelect" class="col-lg-3 selectSubCategories" id="CategoryName">
+			<span class="titleCategoria col-xs-4 col-md-3">Escull categoria:</span>
+			<select name="subCategoriesSelect" class="col-xs-3 selectSubCategories" id="CategoryName">
 				<option value="-1">Categoria</option>
 				<option ng-repeat="category in categories" ng-value="category.idCategory">{{category.name}}</option>
 			</select>
 		</div>
 		<div>
-			<input type="text" class="col-lg-3 inputTextSubCategories" id="idSubC" ng-model="idSubCategorySC" hidden disabled>
-			<input type="text" class="col-lg-3 inputTextSubCategories" id="addAcc" ng-model="accA" hidden disabled>
+			<input type="text" class="col-xs-3 inputTextSubCategories" id="idSubC" ng-model="idSubCategorySC" hidden disabled>
+			<input type="text" class="col-xs-3 inputTextSubCategories" id="addAcc" ng-model="accA" hidden disabled>
 		</div>
 		<div class="row">
-			<span class="titleCategoria col-lg-1">Nom subcategoria:</span>
-			<input type="text" class="col-lg-3 inputTextSubCategories" ng-model="nameSC" id="nsc">
+			<span class="titleCategoria col-xs-4 col-md-3">Nom subcategoria:</span>
+			<input type="text" class="col-xs-3 inputTextSubCategories" ng-model="nameSC" id="nsc">
 		</div>
 		<button id="afegirSubCategoria" class="btn btn-default centerPadding" ng-click="addBBDDSubCat()"><span ng-show="btnName">Afegir</span><span ng-hide="btnName">Editar</span></button>
 	</form>
