e3070b6011e564287dcfeb34232fcb755ff30cdd
Nio94 <neofito94@gmail.com>
2017-11-22 11:02:48 +0100

    update

--- cpanel/js/app.js
+++ cpanel/js/app.js
@@ -2,8 +2,8 @@ var app= angular.module('spaApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
 	.when('/', {
-		templateUrl:'views/settings.php',
-		controller:'SettingsCtrl',
+		templateUrl:'views/promotions.php',
+		controller:'PromotionsCtrl',
 	})
 	.when('/news',{
 	templateUrl:'views/news.php',
