491478d6ebb6c66ae5ea68df113dd5768293c5b3
DidacPH <magozero942.c1@gmail.com>
2017-11-27 08:43:28 +0100

    Parte 1
    if(!isset($_SESSION['user']['idUser'])) header("Location: index.html");
    else if(!isset($_GET['acc'])) { header("Location: ../index.html");}

--- cpanel/models/links.php
+++ cpanel/models/links.php
@@ -2,6 +2,7 @@
 require("../../inc/functions.php");
 session_start();
 if(!isset($_SESSION['user']['idUser'])) header("Location: index.html");
+else if(!isset($_GET['acc'])) { header("Location: ../index.html");}
 
 
 	if (isset($_GET["acc"]) && ($_GET["acc"] == "l"))
