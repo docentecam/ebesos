b0994aeb0721f5c543ade31c42e77a77ca804cc5
Deletion master <aaa>
2017-12-11 11:24:59 +0100

    comerços backend
    cambio de estructura html para adaptarla al responsive de movil.
    boostrap pc y movil hechos

--- cpanel/views/shops.php
+++ cpanel/views/shops.php
@@ -34,68 +34,94 @@
 </div>
 	
 <div class="row" ng-show="showShop">
-	<label class="col-lg-2 title-comerços no-padding">Afegir comerç:</label>
+	<label class="col-lg-12 title-comerços no-padding">Afegir comerç:</label>
 	<form id="i-shop" class="row" name="i-shop">
 		<input type="text" name="" ng-model="shopOne.idShop" hidden>
-		<div class="col-md-12">
-			<span class="col-xs-12 col-lg-12 no-padding">Nom comerç</span>
-			<input class="col-lg-4" type="text" id="n-shop" ng-name="n-shop" ng-model="shopOne.name" placeholder="nom de la tenda" name="" required>
-			<select class="col-lg-4 col-lg-offset-2 select-users" id="u-shop" name="u-shop" ng-change="userOwner(idUser)" ng-model="shopOne.idUser">
-				<option ng-value="userL.idUser" ng-repeat="userL in userList | filter: {idUser:'!1'}" ng-selected="userL.idUser==shopOne.idUser">{{userL.name}}</option>
-			</select>
+		
+			<div class="col-lg-6 col-xs-12">
+				<span class="col-xs-12 col-lg-12 no-padding">Nom comerç</span>
+				<input class="col-lg-8" type="text" id="n-shop" ng-name="n-shop" ng-model="shopOne.name" placeholder="nom de la tenda" name="" required>
+			</div>
+			<div class="col-lg-6 col-xs-12">
+				<select class="col-lg-10 select-users" id="u-shop" name="u-shop" ng-change="userOwner(idUser)" ng-model="shopOne.idUser">
+					<option ng-value="userL.idUser" ng-repeat="userL in userList | filter: {idUser:'!1'}" ng-selected="userL.idUser==shopOne.idUser">{{userL.name}}</option>
+				</select>
+			</div>
+		
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-xs-12 col-lg-12 no-padding">Contrasenya</span>
+			<input class="col-lg-8" type="password" ng-name="ps-shop" ng-model="shopOne.password" placeholder="contrasenya">
 		</div>
-		<div class="col-md-12">
-			<span class="col-xs-12 col-md-12 no-padding">Contrasenya</span>
-			<input class="col-lg-4" type="password" ng-name="ps-shop" ng-model="shopOne.password" placeholder="contrasenya">
+		<div class="col-lg-12 no-padding">
+			<div class="col-lg-6 col-xs-12">
+				<span class="col-xs-12 col-lg-10 no-padding">Descripció llarga</span>
+				<textarea class="col-xs-7 col-lg-8" rows="8" id="descL-shop" name="descL-shop" ng-model="shopOne.descriptionLong">{{shopOne.descriptionLong}}</textarea>
+			</div>
+			<div class="col-lg-6 col-xs-12">
+				<span class="col-xs-6 col-lg-12 no-padding">Descripció curta</span>
+				<textarea class="col-lg-8" rows="4" id="descS-shop" name="descS-shop" ng-model="shopOne.description">{{shopOne.description}}</textarea>
+			</div>
+			<div class="col-lg-6 col-xs-12">
+				<span id="i-shop-city" class="col-lg-12 col-xs-12 no-padding">Ciutat</span>
+				<input class="col-lg-8" type="text" id="c-shop" name="c-shop" placeholder="ciutat" name="" ng-model="shopOne.ciutat">
+			</div>
 		</div>
-		<div class="col-md-12">
-			<span class="col-xs-12 col-lg-6 no-padding">Descripció llarga</span>
-			<span class="col-xs-6 col-lg-6 no-padding">Descripció curta</span>
-			<textarea class="col-xs-7 col-lg-4" rows="10" id="descL-shop" name="descL-shop" ng-model="shopOne.descriptionLong">{{shopOne.descriptionLong}}</textarea>
-			<textarea class="col-lg-4 col-lg-offset-2" rows="4" id="descS-shop" name="descS-shop" ng-model="shopOne.description">{{shopOne.description}}</textarea>
-			<span id="i-shop-city" class="col-lg-4 no-padding col-lg-offset-2">Ciutat</span>
-			<input class="col-lg-4 col-lg-offset-2" type="text" id="c-shop" name="c-shop" placeholder="ciutat" name="" ng-model="shopOne.ciutat">
+		<div class="col-lg-12 col-xs-12 no-padding">
+			<div class="col-lg-6 col-xs-12">
+				<span class="col-lg-12 col-xs-12 no-padding">Logo comerç</span>
+				<label for="lg-shop" class="btn-add col-lg-4">Examinar<i class="fa fa-plus-circle add-examinar" aria-hidden="true"></i></label><input type="file" class="col-lg-4" id="lg-shop" name="lg-shop" uploader-model="shopOne.logo" ng-show="false" onchange="angular.element(this).scope().uploadLogo(this)">
+			</div>
+			<div class="col-lg-6 col-xs-12">
+				<span class="col-lg-12 col-xs-12 no-padding">Pàgina web</span>
+				<input type="text" class="col-lg-8" id="url-shop" name="url-shop" placeholder="www.webtenda.com" name="" ng-model="shopOne.web">
+			</div>
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Logo comerç</span>
-			<span class="col-lg-6 no-padding">Pàgina web</span>
-			<label for="lg-shop" class="btn-add col-lg-2">Examinar<i class="fa fa-plus-circle add-examinar" aria-hidden="true"></i></label><input type="file" class="col-lg-4" id="lg-shop" name="lg-shop" uploader-model="shopOne.logo" ng-show="false" onchange="angular.element(this).scope().uploadLogo(this)">
-			<input type="text" class="col-lg-4 col-lg-offset-4" id="url-shop" name="url-shop" placeholder="www.webtenda.com" name="" ng-model="shopOne.web">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Latitud</span>
+			<input type="text" class="col-lg-8" id="lat-shop" name="lat-shop" placeholder="00.000000" name="" ng-model="shopOne.lat">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Latitud</span>
-			<span class="col-lg-6 no-padding">Longitud</span>
-			<input type="text" class="col-lg-4" id="lat-shop" name="lat-shop" placeholder="00.000000" name="" ng-model="shopOne.lat">
-			<input type="text" class="col-lg-4 col-lg-offset-2" id="lng-shop" name="lng-shop" placeholder="00.000000" name="" ng-model="shopOne.lng">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Longitud</span>
+			<input type="text" class="col-lg-8" id="lng-shop" name="lng-shop" placeholder="00.000000" name="" ng-model="shopOne.lng">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Teléfon</span>
-			<span class="col-lg-6 no-padding">Codi Postal</span>
-			<input type="text" class="col-lg-4" id="p-shop" name="p-shop" placeholder="telefon o móbil" name="" ng-model="shopOne.telephone">
-			<input type="text" class="col-lg-4 col-lg-offset-2" id="cp-shop" name="cp-shop" placeholder="XXXXX" name="" ng-model="shopOne.cp">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Teléfon</span>
+			<input type="text" class="col-lg-8" id="p-shop" name="p-shop" placeholder="telefon o móbil" name="" ng-model="shopOne.telephone">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Adreça</span>
-			<span class="col-lg-6 no-padding">Horari</span>
-			<input type="text" class="col-lg-4" id="a-shop" name="a-shop" placeholder="C/carrer, numero" name="" ng-model="shopOne.address">
-			<input type="text" class="col-lg-4 col-lg-offset-2" id="s-shop" name="s-shop" placeholder="Dl-Dv XX:XX-XX:XX  i de XX:XX-XX:XX" name="" ng-model="shopOne.schedule">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Codi Postal</span>
+			<input type="text" class="col-lg-8" id="cp-shop" name="cp-shop" placeholder="XXXXX" name="" ng-model="shopOne.cp">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Email</span>
-			<span class="col-lg-6 no-padding">Whatsapp</span>
-			<input type="text" class="col-lg-4" id="e-shop" name="e-shop" placeholder="comerç@gmail.com" name="" ng-model="shopOne.email">
-			<input type="text" class="col-lg-4 col-lg-offset-2" id="wa-shop" name="wa-shop" placeholder="telefon o móbil" name="" ng-model="shopOne.userWa">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Adreça</span>
+			<input type="text" class="col-lg-8" id="a-shop" name="a-shop" placeholder="C/carrer, numero" name="" ng-model="shopOne.address">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-6 no-padding">Facebook</span>
-			<span class="col-lg-6 no-padding">Twitter</span>
-			<input type="text" class="col-lg-4" id="fb-shop" name="fb-shop" placeholder="pàgina facebook" name="" ng-model="shopOne.userFb">
-			<input type="text" class="col-lg-4 col-lg-offset-2" id="tt-shop" name="tt-shop" placeholder="pàgina de twitter" name="" ng-model="shopOne.userTt">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Horari</span>
+			<input type="text" class="col-lg-8" id="s-shop" name="s-shop" placeholder="Dl-Dv XX:XX-XX:XX  i de XX:XX-XX:XX" name="" ng-model="shopOne.schedule">
 		</div>
-		<div class="col-md-12">
-			<span class="col-lg-12 no-padding">Instagram</span>
-			<input type="text" class="col-lg-4" id="ig-shop" name="ig-shop" placeholder="pàgina d'instagram" name="" ng-model="shopOne.userIg">
-			<input type="button" class="col-lg-2 col-lg-offset-3 btn-up" value="Actualitzar" ng-click="uploadFile()">
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Email</span>
+			<input type="text" class="col-lg-8" id="e-shop" name="e-shop" placeholder="comerç@gmail.com" name="" ng-model="shopOne.email">
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Whatsapp</span>
+			<input type="text" class="col-lg-8" id="wa-shop" name="wa-shop" placeholder="telefon o móbil" name="" ng-model="shopOne.userWa">
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Facebook</span>
+			<input type="text" class="col-lg-8" id="fb-shop" name="fb-shop" placeholder="pàgina facebook" name="" ng-model="shopOne.userFb">
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Twitter</span>
+			<input type="text" class="col-lg-8" id="tt-shop" name="tt-shop" placeholder="pàgina de twitter" name="" ng-model="shopOne.userTt">
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<span class="col-lg-12 col-xs-12 no-padding">Instagram</span>
+			<input type="text" class="col-lg-8" id="ig-shop" name="ig-shop" placeholder="pàgina d'instagram" name="" ng-model="shopOne.userIg">
+		</div>
+		<div class="col-lg-6 col-xs-12">
+			<input type="button" class="col-lg-4 col-lg-offset-2 btn-up" value="Actualitzar" ng-click="uploadFile()">
 		</div>
 	</form>
 	<div class="row categories-edit" ng-hide="new">
